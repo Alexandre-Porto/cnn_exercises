3b19098337c449b43c0c676f286984116d5d78e8
Alexandre-Porto <vfdiv3@gmail.com>
2020-01-13 22:47:11 -0300

    update

--- keras_mnist.py
+++ keras_mnist.py
@@ -27,11 +27,18 @@ args = vars(ap.parse_args())
 print("[INFO] loading MNIST (full) dataset...") 
 dataset = datasets.fetch_mldata("MNIST original", data_home=None)
 
-# scale the raw pixel intensities to the range [0, 1.0], then 
-# construct the training and testing splits 
-data = dataset.data.astype("float") / 255.0 
-(trainX, testX, trainY, testY) = train_test_split(data, 
-dataset.target, test_size=0.25)
+# load the MNIST dataset and apply min/max scaling to scale the
+# pixel intensity values to the range [0, 1] (each image is
+# represented by an 8 x 8 = 64-dim feature vector)
+print("[INFO] loading MNIST (sample) dataset...")
+digits = datasets.load_digits()
+data = digits.data.astype("float")
+data = (data - data.min()) / (data.max() - data.min())
+print("[INFO] samples: {}, dim: {}".format(data.shape[0],
+data.shape[1]))
+
+# construct the training and testing splits
+(trainX, testX, trainY, testY) = train_test_split(data,digits.target, test_size=0.25)
 
 # convert the labels from integers to vectors 
 lb = LabelBinarizer() 
