1c575b98a64bf4f7b509d12d33cabf4f28866d87
Alexandre-Porto <vfdiv3@gmail.com>
2020-01-15 14:51:43 -0300

    update

--- pyimagesearch/nn/conv/ShallowNet.py
+++ pyimagesearch/nn/conv/ShallowNet.py
@@ -19,18 +19,30 @@ class ShallowNet:
     def build(width, height, depth, classes): 
         # initialize the model along with the input shape to be
         # "channels last" 
-        model = Sequential() 
+        model = Sequential()
+        
+        print('model 1: '+str(model))
+        
         inputShape = (height, width, depth) 
         # if we are using "channels first", update the input shape 
         if K.image_data_format() == "channels_first": 
             inputShape = (depth, height, width)
+            
+            print('model 2: '+str(model))
+
             # define the first (and only) CONV => RELU layer
             model.add(Conv2D(32, (3, 3), padding="same",input_shape=inputShape)) 
             model.add(Activation("relu"))
+            
+            print('model 3: '+str(model))
+            
             # softmax classifier
             model.add(Flatten()) 
             model.add(Dense(classes)) 
             model.add(Activation("softmax"))
+            
+            print('model 4: '+str(model))
+            
             # return the constructed network architecture 
             return model
 
