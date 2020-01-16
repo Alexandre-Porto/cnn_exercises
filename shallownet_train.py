1a2ced6ae1a801fea02c6119e418544de2e1be3e
Alexandre-Porto <vfdiv3@gmail.com>
2020-01-16 11:16:44 -0300

    update

--- shallownet_train.py
+++ shallownet_train.py
@@ -32,7 +32,7 @@ args = vars(ap.parse_args())
 print("[INFO] loading images...") 
 imagePaths = list(paths.list_images(args["dataset"])) 
 # initialize the image preprocessors 
-sp = SimplePreprocessor.SimpleProcessor(32, 32) 
+sp = SimplePreprocessor.SimplePreprocessor(32, 32) 
 iap = ImageToArrayPreprocessor() 
 # load the dataset from disk then scale the raw pixel intensities 
 # to the range [0, 1] 
