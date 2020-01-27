b5dcd49345fd2fec5349c04c7bd366808a2c583b
Alexandre-Porto <vfdiv3@gmail.com>
2020-01-27 17:12:18 -0300

    update

--- train_smile_cnn.py
+++ train_smile_cnn.py
@@ -54,6 +54,8 @@ for imagePath in sorted(list(paths.list_images(args["dataset"]))):
     classTotals = labels.sum(axis=0) 
     classWeight = classTotals.max() / classTotals
     
+    print('data: '+str(data))
+    
     # partition the data into training and testing splits using 80% of 
     # the data for training and the remaining 20% for testing 
     (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.20, stratify=labels, random_state=42)
