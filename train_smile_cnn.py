c24f00449142daf2ab4e7d8dbd5679ed3e757763
Alexandre-Porto <vfdiv3@gmail.com>
2020-01-29 21:45:30 -0300

    update

--- train_smile_cnn.py
+++ train_smile_cnn.py
@@ -45,54 +45,53 @@ for imagePath in sorted(list(paths.list_images(args["dataset"]))):
     label = "smiling" if label == "positives" else "not_smiling" 
     labels.append(label)
     
-    # scale the raw pixel intensities to the range [0, 1] 
-    data = np.array(data, dtype="float") / 255.0 
-    labels = np.array(labels) 
-    # convert the labels from integers to vectors 
-    le = LabelEncoder().fit(labels) 
-    labels = np_utils.to_categorical(le.transform(labels), 2)
-    
-    # account for skew in the labeled data
-    classTotals = labels.sum(axis=0) 
-    classWeight = classTotals.max() / classTotals
-    
-    # print('data: '+str(data))
-    
-    print('data shape: '+str(data.shape))
-    print('labels shape: '+str(labels.shape))
-    
-    # partition the data into training and testing splits using 80% of 
-    # the data for training and the remaining 20% for testing 
-    (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.20, stratify=labels, random_state=42)
-    # initialize the model 
-    print("[INFO] compiling model...") 
-    model = LeNet.build(width=28, height=28, depth=1, classes=2) 
-    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-    # train the network
-    print("[INFO] training network...") 
-    H = model.fit(trainX, trainY, validation_data=(testX, testY), 
-                  class_weight=classWeight, batch_size=64, epochs=15, verbose=1)
-    
-    # evaluate the network 
-    print("[INFO] evaluating network...") 
-    predictions = model.predict(testX, batch_size=64) 
-    print(classification_report(testY.argmax(axis=1), 
-                                predictions.argmax(axis=1), target_names=le.classes_)) 
-    # save the model to disk 
-    print("[INFO] serializing network...") 
-    model.save(args["model"])
-    
-    # plot the training + testing loss and accuracy 
-    plt.style.use("ggplot") 
-    plt.figure() 
-    plt.plot(np.arange(0, 15), H.history["loss"], label="train_loss") 
-    plt.plot(np.arange(0, 15), H.history["val_loss"], label="val_loss") 
-    plt.plot(np.arange(0, 15), H.history["acc"], label="acc") 
-    plt.plot(np.arange(0, 15), H.history["val_acc"], label="val_acc") 
-    plt.title("Training Loss and Accuracy")
-    plt.xlabel("Epoch #") 
-    plt.ylabel("Loss/Accuracy") 
-    plt.legend() 
-    plt.show()
-    
-    
+# scale the raw pixel intensities to the range [0, 1] 
+data = np.array(data, dtype="float") / 255.0 
+labels = np.array(labels) 
+# convert the labels from integers to vectors 
+le = LabelEncoder().fit(labels) 
+labels = np_utils.to_categorical(le.transform(labels), 2)
+
+# account for skew in the labeled data
+classTotals = labels.sum(axis=0) 
+classWeight = classTotals.max() / classTotals
+
+# print('data: '+str(data))
+
+print('data shape: '+str(data.shape))
+print('labels shape: '+str(labels.shape))
+
+# partition the data into training and testing splits using 80% of 
+# the data for training and the remaining 20% for testing 
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.20, stratify=labels, random_state=42)
+# initialize the model 
+print("[INFO] compiling model...") 
+model = LeNet.build(width=28, height=28, depth=1, classes=2) 
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+# train the network
+print("[INFO] training network...") 
+H = model.fit(trainX, trainY, validation_data=(testX, testY), 
+              class_weight=classWeight, batch_size=64, epochs=15, verbose=1)
+
+# evaluate the network 
+print("[INFO] evaluating network...") 
+predictions = model.predict(testX, batch_size=64) 
+print(classification_report(testY.argmax(axis=1), 
+                            predictions.argmax(axis=1), target_names=le.classes_)) 
+# save the model to disk 
+print("[INFO] serializing network...") 
+model.save(args["model"])
+
+# plot the training + testing loss and accuracy 
+plt.style.use("ggplot") 
+plt.figure() 
+plt.plot(np.arange(0, 15), H.history["loss"], label="train_loss") 
+plt.plot(np.arange(0, 15), H.history["val_loss"], label="val_loss") 
+plt.plot(np.arange(0, 15), H.history["acc"], label="acc") 
+plt.plot(np.arange(0, 15), H.history["val_acc"], label="val_acc") 
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #") 
+plt.ylabel("Loss/Accuracy") 
+plt.legend() 
+plt.show()
+
